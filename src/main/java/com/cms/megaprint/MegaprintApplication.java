617febcf8f895f70059ff0f391e5e4d803f0dad4
ILoveMemes <degtyarev_is@mail.ru>
2021-12-16 11:50:12 +0300

    project created

---
+++ src/main/java/com/cms/megaprint/MegaprintApplication.java
@@ -0,0 +1,13 @@
+package com.cms.megaprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cms/megaprint/MegaprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.megaprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
