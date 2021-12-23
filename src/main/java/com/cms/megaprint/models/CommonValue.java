2896cee58e997bb07f34120253a3c82cdda93767
ILoveMemes <degtyarev_is@mail.ru>
2021-12-23 06:35:20 +0300

    added common values model

---
+++ src/main/java/com/cms/megaprint/models/CommonValue.java
@@ -0,0 +1,23 @@
+package com.cms.megaprint.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "common_values")
+@Data
+public class CommonValue {
+
+    @Id
+    @SequenceGenerator(name = "cValuesHS", sequenceName = "c_values_id", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "cValuesHS")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "value")
+    private String value;
+
+}
