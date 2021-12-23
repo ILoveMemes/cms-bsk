277431f08e4f2db4fbbf53f49a590da43cafcaf9
ILoveMemes <degtyarev_is@mail.ru>
2021-12-23 06:54:25 +0300

    added repo for pictures

---
+++ src/main/java/com/cms/megaprint/repos/PictureRepository.java
@@ -0,0 +1,7 @@
+package com.cms.megaprint.repos;
+
+import com.cms.megaprint.models.Picture;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PictureRepository extends CrudRepository<Long, Picture> {
+}
