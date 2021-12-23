96cf648dfe0a59a52d589c791a0ce13fdfd5f540
ILoveMemes <degtyarev_is@mail.ru>
2021-12-23 06:54:08 +0300

    added repo for common values

---
+++ src/main/java/com/cms/megaprint/repos/CommonValueRepository.java
@@ -0,0 +1,7 @@
+package com.cms.megaprint.repos;
+
+import com.cms.megaprint.models.CommonValue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommonValueRepository extends CrudRepository<Long, CommonValue> {
+}
