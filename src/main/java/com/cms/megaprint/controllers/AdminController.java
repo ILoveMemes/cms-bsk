d985b8e95f0b67eb5fde311340d333855094c063
ILoveMemes <degtyarev_is@mail.ru>
2022-01-02 14:54:28 +0300

    added admin controller

---
+++ src/main/java/com/cms/megaprint/controllers/AdminController.java
@@ -0,0 +1,15 @@
+package com.cms.megaprint.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping("/admin")
+    public String admin(Model model) {
+        return "index_admin";
+    }
+
+}
