94a9f2491e94c24822f8504767d72a4fd25944d4
ILoveMemes <degtyarev_is@mail.ru>
2022-01-02 14:59:36 +0300

    added admin controller

--- src/main/java/com/cms/megaprint/controllers/AdminController.java
+++ src/main/java/com/cms/megaprint/controllers/AdminController.java
@@ -7,8 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-    @RequestMapping("/admin")
+    // temporary path - admin2
+    @RequestMapping("/admin2")
     public String admin(Model model) {
+        model.addAttribute("site_caption", "Ремонт компьютеро в Борисоглебске");
+
+        model.addAttribute("top_caption", "МегаПринт | Борисоглебск");
+        model.addAttribute("top_phone", "8 (473 54) 6-58-88");
+        model.addAttribute("top_email","megaprintbsk@mail.ru");
+
+        model.addAttribute("center_welcome", "Добро пожаловать в");
+        model.addAttribute("center_company_name", "МегаПринт");
+        model.addAttribute("center_text", "Мы благодарим Вас за то, что Вы и обратились именно в наш сервисный центр, и мы гарантируем, что Вы не останетесь разочарованны нашим сервисом.");
+        // три колонки в index.html
+
+        model.addAttribute("text", "Hello, World!");
         return "index_admin";
     }
 
