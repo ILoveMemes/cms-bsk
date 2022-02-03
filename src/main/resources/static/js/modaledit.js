ccec7e453cd534b169cd74d7ec8ddf520862dcdb
ILoveMemes <degtyarev_is@mail.ru>
2022-02-03 07:36:01 +0300

    added picture upload dialog

--- src/main/resources/static/js/modaledit.js
+++ src/main/resources/static/js/modaledit.js
@@ -75,3 +75,24 @@ function updateDoc() {
 }
 
 prepareDoc();
+
+function showPicUploadDialog() {
+    $('#modalPicUploadDialog').modal();
+}
+
+function picDialogInit() {
+    $('#modalPicUploadDialogApply').unbind();
+    $('#modalPicUploadDialogApply').click(doUpload);
+}
+
+function doUpload() {
+    console.log('doUpload..');
+    let pic = document.getElementById('modalPicUploadInput').files[0];  // file from input
+    let req = new XMLHttpRequest();
+    let formData = new FormData();
+    formData.append("image", pic);
+    req.open("POST", '/pic/upload');
+    req.send(formData);
+
+    $('#modalPicUploadDialog').modal('hide');
+}
