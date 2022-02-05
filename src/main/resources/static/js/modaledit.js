f370404f37c3e7903f669e8caefd3e104768e9bb
ILoveMemes <degtyarev_is@mail.ru>
2022-02-05 08:30:12 +0300

    picture selection window updated. now contain the picture upload dialog. added delete picture option

--- src/main/resources/static/js/modaledit.js
+++ src/main/resources/static/js/modaledit.js
@@ -76,6 +76,8 @@ function updateDoc() {
 
 prepareDoc();
 
+/////////////////////////////// picture upload dialog ////////////////////////////////////////////
+
 function showPicUploadDialog() {
     $('#modalPicUploadDialog').modal();
 }
@@ -86,28 +88,41 @@ function picDialogInit() {
 }
 
 function doUpload() {
-    console.log('doUpload..');
     let pic = document.getElementById('modalPicUploadInput').files[0];  // file from input
     let req = new XMLHttpRequest();
     let formData = new FormData();
     formData.append("image", pic);
     req.open("POST", '/pic/upload');
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
+            $('#modalPicUploadDialog').modal('hide');
+            picPickDialogRefresh();
+            showPicPickDialog();
+        }
+    };
     req.send(formData);
-
-    $('#modalPicUploadDialog').modal('hide');
 }
+
+function dismissUpload() {
+    $('#modalPicUploadDialog').modal('hide');
+    showPicPickDialog();
+}
+
+//////////////////////////// picture select dialog ////////////////////////////////////
 
 function  showPicPickDialog() {
     $('#modalPicPickDialog').modal();
 }
 
 function picPickDialogRefresh() {
+    var selectedPicture = {id: -1, element: null};
+    $('#modalPickDeleteSelected').attr('disabled', true);
+    var list = document.getElementById('modalPicPickList');
+    list.innerHTML = '';
     var request = new XMLHttpRequest();
     request.open("GET", '/pic/idList', true);
-    request.send(null);
     request.onreadystatechange = function() {
         if (request.readyState == XMLHttpRequest.DONE && request.status == 200 && request.response != null) {
-            let list = document.getElementById('modalPicPickList');
             let data = JSON.parse(request.response);
             data.forEach(i => {
                 let div = document.createElement('div');
@@ -117,7 +132,40 @@ function picPickDialogRefresh() {
                 img.src = '/pic/' + i;
                 img.classList.add('modal-list-picture');
                 div.appendChild(img);
+                div.onclick = () => {
+                    //// click by item
+                    if (selectedPicture.element != null) {
+                        selectedPicture.element.classList.remove('modal-list-item-selected');
+                    }
+                    selectedPicture.id = i;
+                    selectedPicture.element = div;
+                    div.classList.add('modal-list-item-selected');
+                    $('#modalPicPickDialogApply').unbind();
+                    $('#modalPicPickDialogApply').click(() => {
+                        ////// apply selection
+                        $('#modalPicPickDialog').modal('hide');
+                        console.log('now selected: ' + i);
+                    });
+                    $('#modalPickDeleteSelected').attr('disabled', false);
+                    $('#modalPickDeleteSelected').click(() => {
+                        ///// delete picture
+                        let deleteRequest = new XMLHttpRequest();
+                        deleteRequest.open("GET", '/pic/deleteById/' + i, true);
+                        deleteRequest.onreadystatechange = () => {
+                            if (deleteRequest.readyState == XMLHttpRequest.DONE && deleteRequest.status == 200) {
+                                picPickDialogRefresh();
+                            }
+                        };
+                        deleteRequest.send(null);
+                    });
+                };
             });
         }
     };
+    request.send(null);
+}
+
+function doLoadNewPicture() {
+    $('#modalPicPickDialog').modal('hide');
+    showPicUploadDialog();
 }
