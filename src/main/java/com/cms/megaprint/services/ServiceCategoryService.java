52c4da6e99e43fa28a462ef4caad28216bd42ab9
ILoveMemes <degtyarev_is@mail.ru>
2022-02-06 08:56:48 +0300

    added service for "service"

---
+++ src/main/java/com/cms/megaprint/services/ServiceCategoryService.java
@@ -0,0 +1,6 @@
+package com.cms.megaprint.services;
+
+import com.cms.megaprint.models.ServiceCategory;
+
+public interface ServiceCategoryService extends CrudService<ServiceCategory, Long> {
+}
---
+++ src/main/java/com/cms/megaprint/services/ServiceUnitService.java
@@ -0,0 +1,6 @@
+package com.cms.megaprint.services;
+
+import com.cms.megaprint.models.ServiceUnit;
+
+public interface ServiceUnitService extends CrudService<ServiceUnit, Long>{
+}
