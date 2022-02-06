ef8019604dc7cbcba8f343ccbcf333c1554ce00a
ILoveMemes <degtyarev_is@mail.ru>
2022-02-06 08:53:05 +0300

    added repositories for service

---
+++ src/main/java/com/cms/megaprint/repos/ServiceCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.cms.megaprint.repos;
+
+import com.cms.megaprint.models.ServiceCategory;
+
+public interface ServiceCategoryRepository extends CrudRepo<ServiceCategory, Long> {
+}
---
+++ src/main/java/com/cms/megaprint/repos/ServiceUnitRepository.java
@@ -0,0 +1,6 @@
+package com.cms.megaprint.repos;
+
+import com.cms.megaprint.models.ServiceUnit;
+
+public interface ServiceUnitRepository extends CrudRepo<ServiceUnit, Long>{
+}
