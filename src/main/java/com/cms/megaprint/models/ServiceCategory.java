8ce35fde7de59d46f3e5af4684b41d6dc3bd4f5d
ILoveMemes <degtyarev_is@mail.ru>
2022-02-06 08:52:44 +0300

    added service unit and category entities

---
+++ src/main/java/com/cms/megaprint/models/ServiceCategory.java
@@ -0,0 +1,27 @@
+package com.cms.megaprint.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "service_category")
+@Data
+public class ServiceCategory {
+
+    @Id
+    @SequenceGenerator(name = "serviceCategoryHS", sequenceName = "service_category_id", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "serviceCategoryHS")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "pictureId")
+    private Long pictureId;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "service_category")
+    List<ServiceUnit> units;
+
+}
---
+++ src/main/java/com/cms/megaprint/models/ServiceUnit.java
@@ -0,0 +1,30 @@
+package com.cms.megaprint.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "service_unit")
+@Data
+public class ServiceUnit {
+
+    @Id
+    @SequenceGenerator(name = "serviceUnitHS", sequenceName = "service_unit_id", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "serviceUnitHS")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "showOnMain")
+    private boolean showOnMain;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Long category;
+
+}
