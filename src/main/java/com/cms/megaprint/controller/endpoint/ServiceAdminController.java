d2b1468b2c3de72da770cd8c815978465288c895
ILoveMemes <degtyarev_is@mail.ru>
2022-03-31 21:19:49 +0300

    now unit without categories info is transfer to front

--- src/main/java/com/cms/megaprint/controller/endpoint/ServiceAdminController.java
+++ src/main/java/com/cms/megaprint/controller/endpoint/ServiceAdminController.java
@@ -1,12 +1,16 @@
 package com.cms.megaprint.controller.endpoint;
 
 import com.cms.megaprint.model.CommonValue;
+import com.cms.megaprint.model.ServiceUnit;
 import com.cms.megaprint.service.intface.CommonValueService;
 import com.cms.megaprint.service.intface.ServiceCategoryService;
 import com.cms.megaprint.service.common.TextDecoratorService;
+import com.cms.megaprint.service.intface.ServiceUnitService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ServiceAdminController {
@@ -14,11 +18,13 @@ public class ServiceAdminController {
     private final CommonValueService commonValueService;
     private final TextDecoratorService textDecoratorService;
     private final ServiceCategoryService serviceCategoryService;
+    private final ServiceUnitService serviceUnitService;
 
-    public ServiceAdminController(CommonValueService commonValueService, TextDecoratorService textDecoratorService, ServiceCategoryService serviceCategoryService) {
+    public ServiceAdminController(CommonValueService commonValueService, TextDecoratorService textDecoratorService, ServiceCategoryService serviceCategoryService, ServiceUnitService serviceUnitService) {
         this.commonValueService = commonValueService;
         this.textDecoratorService = textDecoratorService;
         this.serviceCategoryService = serviceCategoryService;
+        this.serviceUnitService = serviceUnitService;
     }
 
     @RequestMapping("admin_service")
@@ -28,6 +34,10 @@ public class ServiceAdminController {
             model.addAttribute(cValue.getKey(), textDecoratorService.decorate(cValue.getValue()));
         }
 
+        List<ServiceUnit>  units = serviceUnitService.findUnitsWithoutCategories();
+
+        model.addAttribute("haveUnitsWithoutCategories", units.size() > 0);
+        model.addAttribute("unitsWithoutCategories", units);
         model.addAttribute("serviceCategories", serviceCategoryService.findAll());
 
         return "service_admin";
