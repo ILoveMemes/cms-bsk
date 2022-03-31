10f9ef7f4a90a01a924b217ae28754c89e2ff8d4
ILoveMemes <degtyarev_is@mail.ru>
2022-03-31 21:01:03 +0300

    added selection units by missing category

--- src/main/java/com/cms/megaprint/repository/intface/ServiceUnitRepository.java
+++ src/main/java/com/cms/megaprint/repository/intface/ServiceUnitRepository.java
@@ -2,5 +2,10 @@ package com.cms.megaprint.repository.intface;
 
 import com.cms.megaprint.model.ServiceUnit;
 
+import java.util.List;
+
 public interface ServiceUnitRepository extends CrudRepo<ServiceUnit, Long>{
+
+    List<ServiceUnit> findUnitsWithoutCategories();
+
 }
--- src/main/java/com/cms/megaprint/service/intface/ServiceUnitService.java
+++ src/main/java/com/cms/megaprint/service/intface/ServiceUnitService.java
@@ -3,5 +3,10 @@ package com.cms.megaprint.service.intface;
 import com.cms.megaprint.model.ServiceUnit;
 import com.cms.megaprint.service.common.CrudService;
 
+import java.util.List;
+
 public interface ServiceUnitService extends CrudService<ServiceUnit, Long> {
+
+    List<ServiceUnit> findUnitsWithoutCategories();
+
 }
