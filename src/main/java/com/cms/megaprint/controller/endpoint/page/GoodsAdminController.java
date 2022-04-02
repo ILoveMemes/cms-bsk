b13a4d6fff637371f5dcea08cb4b1259accfb3b0
ILoveMemes <degtyarev_is@mail.ru>
2022-04-02 13:01:44 +0300

    added support for description text formatting (using custom MD)

--- src/main/java/com/cms/megaprint/controller/endpoint/page/GoodsAdminController.java
+++ src/main/java/com/cms/megaprint/controller/endpoint/page/GoodsAdminController.java
@@ -1,12 +1,18 @@
 package com.cms.megaprint.controller.endpoint.page;
 
 import com.cms.megaprint.model.CommonValue;
+import com.cms.megaprint.model.Goods;
 import com.cms.megaprint.service.common.TextDecoratorService;
 import com.cms.megaprint.service.intface.CommonValueService;
 import com.cms.megaprint.service.intface.GoodsService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class GoodsAdminController {
@@ -21,6 +27,13 @@ public class GoodsAdminController {
         this.textDecoratorService = textDecoratorService;
     }
 
+    @Data
+    @AllArgsConstructor
+    private class Description {
+        private String shrt;
+        private String full;
+    }
+
     @RequestMapping("admin_goods")
     public String goods(Model model) {
 
@@ -29,6 +42,19 @@ public class GoodsAdminController {
         }
 
         model.addAttribute("goods", goodsService.findAll());
+
+        Map<Long, Description> goodsDescription = new HashMap<>();
+        for (Goods goods: goodsService.findAll()) {
+            goodsDescription.put(
+                    goods.getId(),
+                    new Description(
+                            textDecoratorService.decorate(goods.getShortDescription()),
+                            textDecoratorService.decorate(goods.getFullDescription())
+                    )
+            );
+        }
+
+        model.addAttribute("goodsFormattedDescription", goodsDescription);
 
         return "goods_admin";
     }
