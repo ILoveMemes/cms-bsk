97319a6b9a220801205251a9541f133e4a844ed8
ILoveMemes <degtyarev_is@mail.ru>
2022-04-02 09:44:01 +0300

    Goods repository error fixed

--- src/main/java/com/cms/megaprint/repository/implementation/GoodsRepositoryImpl.java
+++ src/main/java/com/cms/megaprint/repository/implementation/GoodsRepositoryImpl.java
@@ -4,7 +4,9 @@ import com.cms.megaprint.db.HibernateSessionFactoryUtil;
 import com.cms.megaprint.model.Goods;
 import com.cms.megaprint.repository.implementation.hibernate.CrudRepoHibernateImpl;
 import com.cms.megaprint.repository.intface.GoodsRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GoodsRepositoryImpl extends CrudRepoHibernateImpl<Goods, Long> implements GoodsRepository {
     public GoodsRepositoryImpl(HibernateSessionFactoryUtil hibernateSessionFactoryUtil) {
         super(hibernateSessionFactoryUtil);
