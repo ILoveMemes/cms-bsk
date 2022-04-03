e447f49b4c544b543b08af3d4d632416646ebb2f
ILoveMemes <degtyarev_is@mail.ru>
2022-04-03 13:48:49 +0300

    added message administration common page controller

---
+++ src/main/java/com/cms/megaprint/controller/endpoint/page/MessageAdminController.java
@@ -0,0 +1,25 @@
+package com.cms.megaprint.controller.endpoint.page;
+
+import com.cms.megaprint.service.intface.CommonValueService;
+import com.cms.megaprint.service.intface.MessageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MessageAdminController {
+
+    private final MessageService messageService;
+
+    public MessageAdminController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @RequestMapping("/admin_messages")
+    public String messages(Model model) {
+
+        model.addAttribute("messages", messageService.findAll());
+
+        return "messages_admin";
+    }
+}
