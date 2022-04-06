24297fc59915e043d4bd19169c62bf675c62d3fe
ILoveMemes <degtyarev_is@mail.ru>
2022-04-06 22:06:09 +0300

    relocated SiteSection updating code

--- src/main/java/com/cms/megaprint/configuration/EmbedServerCustomConfiguration.java
+++ src/main/java/com/cms/megaprint/configuration/EmbedServerCustomConfiguration.java
@@ -1,24 +1,61 @@
 package com.cms.megaprint.configuration;
 
 
+import com.cms.megaprint.model.CommonValue;
+import com.cms.megaprint.service.intface.CommonValueService;
 import org.springframework.stereotype.Component;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
 
 @Component
 public class EmbedServerCustomConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
 
     private final VarConfig varConfig;
+    private final CommonValueService commonValueService;
 
-    public EmbedServerCustomConfiguration(VarConfig varConfig) {
+    public EmbedServerCustomConfiguration(VarConfig varConfig, CommonValueService commonValueService) {
         this.varConfig = varConfig;
+        this.commonValueService = commonValueService;
+    }
+
+    private void refreshSiteSection(String sectionName) {
+        Optional<CommonValue> vCaption;
+        Optional<CommonValue> vVisible;
+        boolean anyChange;
+
+        anyChange = false;
+        SiteSection siteSection = varConfig.getSiteSections().get(sectionName);
+        vCaption = commonValueService.findByKey("site_section_" + sectionName + "_caption");
+        if (vCaption.isPresent()) {
+            siteSection.setCaption(vCaption.get().getValue());
+            anyChange = true;
+        }
+        vVisible = commonValueService.findByKey("site_section_" + sectionName + "_visible");
+        if (vVisible.isPresent()) {
+            siteSection.setVisible(Boolean.parseBoolean(vVisible.get().getValue()));
+            anyChange = true;
+        }
+        if (anyChange) {
+            varConfig.getSiteSections().put(sectionName, siteSection);
+        }
     }
 
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
+
+        // refresh configuration before server starts
+        refreshSiteSection("home");
+        refreshSiteSection("service");
+        refreshSiteSection("teammate");
+        refreshSiteSection("goods");
+        refreshSiteSection("contact");
+        refreshSiteSection("certificate");
+        refreshSiteSection("carousel");
+
         factory.setPort(varConfig.getPort());
         try {
             factory.setAddress(InetAddress.getByName(varConfig.getAddress()));
--- src/main/java/com/cms/megaprint/controller/endpoint/page/IndexController.java
+++ src/main/java/com/cms/megaprint/controller/endpoint/page/IndexController.java
@@ -43,39 +43,9 @@ public class IndexController {
         this.certificateService = certificateService;
     }
 
-    private void refreshSiteSection(String sectionName) {
-        Optional<CommonValue> vCaption;
-        Optional<CommonValue> vVisible;
-        boolean anyChange;
-
-        anyChange = false;
-        SiteSection siteSection = varConfig.getSiteSections().get(sectionName);
-        vCaption = commonValueService.findByKey("site_section_" + sectionName + "_caption");
-        if (vCaption.isPresent()) {
-            siteSection.setCaption(vCaption.get().getValue());
-            anyChange = true;
-        }
-        vVisible = commonValueService.findByKey("site_section_" + sectionName + "_visible");
-        if (vVisible.isPresent()) {
-            siteSection.setVisible(Boolean.parseBoolean(vVisible.get().getValue()));
-            anyChange = true;
-        }
-        if (anyChange) {
-            varConfig.getSiteSections().put(sectionName, siteSection);
-        }
-    }
-
     @RequestMapping({"", "/", "index", "index.html"})
     public String index(Model model) {
 
-        // refresh SiteSection configuration according to db
-        refreshSiteSection("home");
-        refreshSiteSection("service");
-        refreshSiteSection("teammate");
-        refreshSiteSection("goods");
-        refreshSiteSection("contact");
-        refreshSiteSection("certificate");
-        refreshSiteSection("carousel");
         model.addAttribute("siteSections", varConfig.getSiteSections());
 
         for (CommonValue cValue: commonValueService.findAll()) {
