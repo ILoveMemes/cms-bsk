dc9cd1af8955d8b519637abc19b818bf2a1901e0
ILoveMemes <degtyarev_is@mail.ru>
2022-04-06 20:48:26 +0300

    added SiteSection model into variable config

--- src/main/java/com/cms/megaprint/configuration/VarConfig.java
+++ src/main/java/com/cms/megaprint/configuration/VarConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import java.util.HashMap;
+
 @ConstructorBinding
 @ConfigurationProperties("mp")
 @RequiredArgsConstructor
@@ -17,5 +19,6 @@ public class VarConfig {
     private final String dbPass;
     private final int port;
     private final String address;
+    private final HashMap<String, SiteSection> siteSections;
 
 }
