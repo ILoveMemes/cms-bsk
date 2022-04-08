d1b67eea7293578eba462d7a2304378c5ef4e64d
ILoveMemes <degtyarev_is@mail.ru>
2022-04-08 18:21:03 +0300

    removed unused code

--- src/main/java/com/cms/megaprint/configuration/EmbedServerCustomConfiguration.java
+++ src/main/java/com/cms/megaprint/configuration/EmbedServerCustomConfiguration.java
@@ -22,40 +22,8 @@ public class EmbedServerCustomConfiguration implements WebServerFactoryCustomize
         this.commonValueService = commonValueService;
     }
 
-    /*private void refreshSiteSection(String sectionName) {
-        Optional<CommonValue> vCaption;
-        Optional<CommonValue> vVisible;
-        boolean anyChange;
-
-        anyChange = false;
-        SiteSection siteSection = varConfig.getSiteSections().get(sectionName);
-        vCaption = commonValueService.findByKey("site_section_" + sectionName + "_caption");
-        if (vCaption.isPresent()) {
-            siteSection.setCaption(vCaption.get().getValue());
-            anyChange = true;
-        }
-        vVisible = commonValueService.findByKey("site_section_" + sectionName + "_visible");
-        if (vVisible.isPresent()) {
-            siteSection.setVisible(Boolean.parseBoolean(vVisible.get().getValue()));
-            anyChange = true;
-        }
-        if (anyChange) {
-            varConfig.getSiteSections().put(sectionName, siteSection);
-        }
-    }*/
-
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
-
-        // refresh configuration before server starts
-        /*refreshSiteSection("home");
-        refreshSiteSection("service");
-        refreshSiteSection("teammate");
-        refreshSiteSection("goods");
-        refreshSiteSection("contact");
-        refreshSiteSection("certificate");
-        refreshSiteSection("carousel");*/
-
         factory.setPort(varConfig.getPort());
         try {
             factory.setAddress(InetAddress.getByName(varConfig.getAddress()));
