398926717a97cd4175716b3c1d1a1314f14875b5
ILoveMemes <degtyarev_is@mail.ru>
2022-04-08 20:15:30 +0300

    added find by key method

--- src/main/java/com/cms/megaprint/controller/endpoint/crud/SiteSectionController.java
+++ src/main/java/com/cms/megaprint/controller/endpoint/crud/SiteSectionController.java
@@ -1,8 +1,11 @@
 package com.cms.megaprint.controller.endpoint.crud;
 
+import com.cms.megaprint.exception.NotFoundException;
+import com.cms.megaprint.model.CommonValue;
 import com.cms.megaprint.model.SiteSection;
 import com.cms.megaprint.controller.common.CrudController;
 import com.cms.megaprint.service.common.CrudService;
+import com.cms.megaprint.service.intface.CommonValueService;
 import com.cms.megaprint.service.intface.SiteSectionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +29,11 @@ public class SiteSectionController extends CrudController<SiteSection, Long> {
             service.update(entity.get());
         }
         return object;
+    }
+
+    @GetMapping("findByKey/{key}")
+    public SiteSection findByKey(@PathVariable String key) {
+        return ((SiteSectionService) service).findByKey(key).orElse(null);
     }
 
 //    for debug purposes
